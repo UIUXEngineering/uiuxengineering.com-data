4257135ee3c040741c9770222b1982c8b7b93635
Jerry Orta <jerryorta.dev@gmail.com>
2015-08-10 04:10:07 -0500

    fix schema validation errors

---
+++ schemas/base-schema.js
@@ -0,0 +1,63 @@
+/**
+ * Created by jerryorta-dev on 8/9/15.
+ */
+
+module.exports = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "UIUXEngineering",
+    "description": "UIUX Engineering Data Schema",
+    "id": "UIUXDataObject",
+    "type": "object",
+    "properties": {
+        "title": {
+            "type": "string",
+            "description": "Term to be defined."
+        },
+        "description": {
+            "type": "string",
+            "description": "Definition of term."
+        },
+        "references": {
+            "type": "array",
+            "description": "array of url titles and links.",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "label": { "type": "string" },
+                    "url": { "type": "string" }
+                },
+                "required": [ "label", "url" ]
+            }
+
+
+        },
+        "seed": {
+            "type": "object",
+            "description": "Code repository providing starter environment.",
+            "properties": {
+                "label": { "type": "string" },
+                "server": {
+                    "type": "string",
+                    "description": "github, bitbucket, tfs"
+                },
+                "repotype": {
+                    "type": "string",
+                    "description": "git, svn"
+                },
+                "url": { "type": "string" }
+            },
+            "required": [ "label", "server", "repoType", "url" ]
+        },
+        "tags": {
+            "type": "array",
+            "description": "taxonomy words or phrases",
+            "items": { "type": "string"}
+        },
+        "dataSet": {
+            "type": "string",
+            "description": "url to data json without the .json extension. For Example: angular2/angular2-articles"
+        },
+        "show": {"type": "boolean"}
+    },
+    "required": [ "title", "description", "references", "seed", "tags", "dataSet", "show" ]
+};
